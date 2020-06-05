b17e5e84c819b86ff6f28a647acbf7ff150d5996
bulang <1107842278@qq.com>
2020-06-05 17:10:50 +0800

    新增二叉搜索树

---
+++ src/structures/BinaryTree/index.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: liu wang wang
+ * @Date: 2020-06-02 15:08:56
+ * @LastEditTime: 2020-06-05 17:09:30
+ * @LastEditors: liu wang wang
+ * @Description: 二叉搜索树
+ * @FilePath: \DataStructuresAndAlgorithms\src\structures\BinaryTree\index.js
+ */ 
+class Node{
+    constructor(){
+        
+    }
+}
